0b80ce0570d88b51f0fa2e91e2343f3b385e5fbc
Angel Raul Acosta Rojas <angelr.acosta@softtekprojects.com>
2021-08-24 14:32:42 -0500

    pruebas con un hola mundo

--- formulario.php
+++ formulario.php
@@ -1,27 +1,45 @@
 <?php 
 
-$destino = 'raulacostaservicio@hotmail.com';
+// $destino = 'raulacostaservicio@hotmail.com';
 
-$asunto = 'ALARMA_ONLINE_MENSAJE';
-$mensaje = $_POST ['mensaje'];
-$email = $_POST ['email'];
+// $asunto = "ALARMA_ONLINE_MENSAJE";
+// $mensaje = $_POST ['mensaje'];
+// $email = $_POST ['email'];
 
-$header = 'Mensaje desde Aplicación Alarma Online';
+// $header = "Mensaje desde Aplicación Alarma Online";
 
-mail($destino, $asunto, $mensaje, $header);
+// mail($destino, $asunto, $mensaje, $header);
 
 // header("Location:index.html");
-echo "
-<script>
-    alert('Mensaje Enviado');
-    console.log("???");
 
-</script>"
-echo "
-<script>
-    setTimeout(\"location.href = 'index.html' \",1000);
-    console.log("???");
-</script>
-"
-
+// $to = "destinatario@email.com";
+// $subject = "Asunto del email";
+// $headers = "MIME-Version: 1.0" . "\r\n";
+// $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+ 
+// $message = "
+// <html>
+// <head>
+// <title>HTML</title>
+// </head>
+// <body>
+// <h1>Esto es un H1</h1>
+// <p>Esto es un párrafo en HTML</p>
+// </body>
+// </html>";
+ 
+// mail($to, $subject, $message, $headers);
+echo '<p>Hola Mundo</p>';
+// echo "
+// <script>
+//     alert('Mensaje Enviado');
+//     console.log("???");
+// </script>"
+// echo "
+// <script>
+//     setTimeout(\"location.href = 'index.html' \",1000);
+//     console.log("???");
+// </script>
+// "
+// header("location: index.html"); 
 ?>
